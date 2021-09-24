c71e7194f83afe3b8c0b867ff4992aaf894f94f2
murtaza98 <murtaza.patrawala@rocket.chat>
2021-09-24 15:16:33 +0530

    genesis

---
+++ endpoints/FulfillmentEndpoint.ts
@@ -0,0 +1,18 @@
+import { IHttp, IModify, IPersistence, IRead } from '@rocket.chat/apps-engine/definition/accessors';
+import { ApiEndpoint, IApiEndpointInfo, IApiRequest, IApiResponse } from '@rocket.chat/apps-engine/definition/api';
+
+export class FulfillmentEndpoint extends ApiEndpoint {
+    public path = 'fulfillment';
+
+    public async post(request: IApiRequest,
+                      endpoint: IApiEndpointInfo,
+                      read: IRead,
+                      modify: IModify,
+                      http: IHttp,
+                      persis: IPersistence): Promise<IApiResponse> {
+
+        console.log('---request', request.content);
+
+        return this.success();
+    }
+}
